4ba89ce845a212c552d92ac9b96a5505045de926
Teki <w2495969292@gmail.com>
2024-07-02 21:45:18 +0900

    test: add api route to test ip retrival

---
+++ app/api/ai/route.ts
@@ -0,0 +1,38 @@
+import { NextRequest } from "next/server";
+import { headers } from "next/headers";
+const rateLimitMap = new Map();
+export async function GET(req: Request) {
+  // const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+  const headersList = headers()
+  const ip = headersList.get('request-ip')
+  const geo = headersList.get('request-geo') 
+  console.log("ip: " + ip)
+  console.log("geo:" + geo)
+  req.headers.forEach(header => {
+    console.log(header)
+  })
+
+  const limit = 3; // Limiting requests to 5 per minute per IP
+  const windowMs = 60 * 1000; // 1 minute
+  
+  if (!rateLimitMap.has(ip)) {
+      rateLimitMap.set(ip, {
+          count: 0,
+          lastReset: Date.now(),
+      });
+  }
+  
+  const ipData = rateLimitMap.get(ip);
+  
+  if (Date.now() - ipData.lastReset > windowMs) {
+      ipData.count = 0;
+      ipData.lastReset = Date.now();
+  }
+  
+  if (ipData.count >= limit) {
+    return new Response(JSON.stringify({ hello: "to many responses" }))
+  }
+  
+  ipData.count += 1;
+  return new Response(JSON.stringify({ hello: "hello" }));
+}
